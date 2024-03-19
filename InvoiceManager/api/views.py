d393ea59eb6ce978596f1ac2c07468d6673c725b
divyjx <divyjx@gmail.com>
2024-03-19 12:25:38 +0530

    views updated

--- InvoiceManager/api/views.py
+++ InvoiceManager/api/views.py
@@ -10,27 +10,24 @@ class InvoiceViewSet(ModelViewSet):
     queryset = Invoice.objects.all()
     serializer_class = InvoiceSerializer
 
+    def fill_response(self, invoice_data, detail_data = None):
+        return {
+                    "invoice": invoice_data,
+                    "details": detail_data
+                }
     
-
     def list(self, request, *args, **kwargs):
         print("LIST")
         data = []
         for invoice in self.get_queryset():
             invoice_data = self.get_serializer(invoice).data
             invoice_detail = InvoiceDetail.objects.filter(invoice=invoice).first()
+            details_data = None
             if invoice_detail:
                 details_ser = InvoiceDetailSerializer(instance=invoice_detail)
                 details_data = details_ser.data
-
-                data.append({
-                    "invoice": invoice_data,
-                    "details": details_data
-                })
-            else :
-                data.append({
-                    "invoice": invoice_data,
-                    "details": None
-                })
+        
+            data.append(self.fill_response(invoice_data, details_data))
         return Response(data)
     
     def create(self, request):
@@ -38,20 +35,14 @@ class InvoiceViewSet(ModelViewSet):
         serializer = self.get_serializer(data=request.data)
         details_data = request.data.get('details', None)
         if serializer.is_valid():
+            invoice = serializer.save()
             if details_data:
-                invoice = serializer.save()
                 details_data["invoice"] = invoice.id
-                invoice_detail_serializer = InvoiceDetailSerializer(data = details_data)
-                if invoice_detail_serializer.is_valid():
-                    invoice_detail_serializer.save()
-                    return Response({
-                        "invoice":serializer.data,
-                        "details":invoice_detail_serializer.data
-                    })
-                else:
-                    return Response(invoice_detail_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            else:
-                serializer.save()
+                detail_serializer = InvoiceDetailSerializer(data = details_data)
+                if detail_serializer.is_valid():
+                    detail_serializer.save()
+                    return Response(self.fill_response(serializer.data, detail_serializer.data)
+                        , status=status.HTTP_201_CREATED)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -62,90 +53,44 @@ class InvoiceViewSet(ModelViewSet):
         instance = get_object_or_404(queryset, pk=pk)  
         serializer = self.get_serializer(instance)  # Serialize instance
         invoice_data = serializer.data
-        invoice_detail = InvoiceDetail.objects.filter(invoice=instance).first()
+        invoice_detail = InvoiceDetail.objects.filter(invoice=pk).first()
+        detail_data = None
         if invoice_detail:
             details_ser = InvoiceDetailSerializer(instance=invoice_detail)
-            details_data = details_ser.data
+            detail_data = details_ser.data
 
-            data = {
-                "invoice": invoice_data,
-                "details": details_data
-            }
-        else:
-            data = {
-                "invoice": invoice_data,
-                "details": None
-            }
-        return Response(data)    
+        return Response(self.fill_response(invoice_data, detail_data))    
     
+    def update_or_partial_update(self, request, pk=None, partial=False):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        if serializer.is_valid():
+            serializer.save()
+
+            invoice_detail = InvoiceDetail.objects.filter(invoice=pk).first()
+            detail_data = None
+            if invoice_detail:
+                data = request.data.get('details')
+                data["invoice"] = pk
+                detail_serializer = InvoiceDetailSerializer(instance=invoice_detail, data=data)
+                if detail_serializer.is_valid():
+                    detail_serializer.save()
+                    detail_data = detail_serializer.data
+            
+            return Response(self.fill_response(serializer.data, detail_data))
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def update(self, request, pk=None):
         print("UPDATE")
-        instance = self.get_object()  
-        serializer = self.get_serializer(instance, data=request.data)  
-        if serializer.is_valid():
-            serializer.save()
-            invoice_detail = InvoiceDetail.objects.filter(invoice=pk).first()
-            
-            data = request.data.get('details')
-            data["invoice"] = pk
-            if invoice_detail:
-                detail_serializer = InvoiceDetailSerializer(instance=invoice_detail, 
-                                                            data=data)
-                if detail_serializer.is_valid():
-                    detail_serializer.save()
-                else:
-                    return Response(detail_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            
-            invoice_data = serializer.data
-            details_data = None
-            if invoice_detail:
-                details_data = detail_serializer.data
-            
-            data = {
-                "invoice": invoice_data,
-                "details": details_data
-            }
-            
-            return Response(data)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return self.update_or_partial_update(request, pk=pk)
 
-  
-    
     def partial_update(self, request, pk=None):
         print("PARTIAL_UPDATE")
-        instance = self.get_object()  
-        serializer = self.get_serializer(instance, data=request.data)  
-        if serializer.is_valid():
-            serializer.save()
-            invoice_detail = InvoiceDetail.objects.filter(invoice=pk).first()
-            
-            data = request.data.get('details')
-            data["invoice"] = pk
-            if invoice_detail:
-                detail_serializer = InvoiceDetailSerializer(instance=invoice_detail, 
-                                                            data=data)
-                if detail_serializer.is_valid():
-                    detail_serializer.save()
-                else:
-                    return Response(detail_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            
-            invoice_data = serializer.data
-            details_data = None
-            if invoice_detail:
-                details_data = detail_serializer.data
-            
-            data = {
-                "invoice": invoice_data,
-                "details": details_data
-            }
-            
-            return Response(data)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+        return self.update_or_partial_update(request, pk=pk, partial=True)
+   
     def destroy(self, request, pk=None):
         print("DESTROY")
-        instance = self.get_object()  # Get the instance to be deleted
-        instance.delete()  # Delete the instance
-        return Response(status=status.HTTP_204_NO_CONTENT)  # Return 204 status code for successful deletion
+        instance = self.get_object()  
+        instance.delete()  
+        return Response(status=status.HTTP_204_NO_CONTENT)
